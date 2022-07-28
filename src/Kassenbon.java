36eae56821a53f6400038027727f3d53bb6fe1c2
GoldGoblin <niki.wie.2000@gmail.com>
2022-07-28 15:39:34 +0200

    kassenbon

--- src/Kassenbon.java
+++ src/Kassenbon.java
@@ -1,81 +1,133 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Kassenbon {
 
     public static void main(String[] args) {
 
-        double preisWurst;
-        preisWurst = 4.20;
+        Scanner scanner = new Scanner(System.in);
 
-        double preisKaese;
-        preisKaese = 2.30;
+        List<Produkt> produkts = new ArrayList<>();
 
-        double preisBrot;
-        preisBrot = 2.10;
+        Produkt wurst = new Produkt( "Wurst", 4.20);
+        produkts.add(wurst);
 
-        double preisDVD;
-        preisDVD = 12.00;
+        Produkt kaese = new Produkt("Kaese", 2.30);
+        produkts.add(kaese);
 
-        int anzahlWurst;
-        anzahlWurst = 1;
+        Produkt brot = new Produkt("Brot",2.10);
+        produkts.add(brot);
 
-        int anzahlKaese;
-        anzahlKaese = 1;
-
-        int anzahlBrot;
-        anzahlBrot = 1;
-
-        int anzahlDVD;
-        anzahlDVD = 2;
-
-        double preisgesammtWurst;
-        preisgesammtWurst = anzahlWurst * preisWurst;
-
-        double preisgesammtKaese;
-        preisgesammtKaese = anzahlKaese * preisKaese;
-
-        double preisgesammtBrot;
-        preisgesammtBrot = anzahlBrot * preisBrot;
-
-        double preisgesammtDVD;
-        preisgesammtDVD = anzahlDVD * preisDVD;
-
-        double inhaltBrieftache;
-        inhaltBrieftache = 50.00;
-
-        double preisGesammt;
-        preisGesammt = (preisgesammtWurst + preisgesammtKaese + preisgesammtBrot + preisgesammtDVD);
+        Produkt dvd = new Produkt("DVD", 12.00);
+        produkts.add(dvd);
 
 
-        if (preisGesammt > inhaltBrieftache) {
+   //     String nameProdukt = scanner.nextLine();
+     //   int anzahlProdukt = scanner.nextInt();
 
-            System.out.println("-----------------------------------------------------------");
-            System.out.println(" ihnen fehlen " + (preisGesammt - inhaltBrieftache) + " EUR um diese Artikel zu Kaufen");
-            System.out.println("-----------------------------------------------------------");
+      //  System.out.println("sie haben " +nameProdukt + anzahlProdukt );
 
-        } else {
 
-            System.out.println("---------------------Beleg---------------------");
+        System.out.println("welches produkt haben sie gekauft");
+        String nameProdukt1 = scanner.nextLine();
 
-            System.out.println("Wurst" + "   " + anzahlWurst + " x " + preisWurst + " EUR");
-            System.out.println("                     " + preisgesammtWurst + " EUR");
 
-            System.out.println("Kaese" + "   " + anzahlKaese + " x " + preisKaese + " EUR");
-            System.out.println("                     " + preisgesammtKaese + " EUR");
+        System.out.println("Wie viel haben sie von dem Produkt gekauft");
+        Integer anzahlProdukt1 = scanner.nextInt();
 
-            System.out.println("Brot " + "   "  + anzahlBrot + " x " + preisBrot + " EUR");
-            System.out.println("                     " + preisgesammtBrot + " EUR");
+        System.out.println("sie haben "+ anzahlProdukt1 + nameProdukt1  );
 
-            System.out.println("DVD  " + "   "  + anzahlDVD + " x " + preisDVD + " EUR");
-            System.out.println("                     " + preisgesammtDVD + " EUR");
 
-            System.out.println("----------------------------------------------");
 
-            System.out.println("Gesamt " + "              " + preisGesammt + " EUR" );
-            System.out.println("Gegeben" + "              " +  inhaltBrieftache + " EUR" );
-            System.out.println( " ");
-            System.out.println("Zurueck" + "              " + (inhaltBrieftache - preisGesammt) + " EUR");
+        System.out.println("welches produkt haben sie gekauft");
+        String nameProdukt2 = scanner.nextLine();
 
-        }
+     //   System.out.println("Wie viel haben sie von dem Produkt gekauft");
+     //   Integar anzahlProdukt2 = scanner.nextInt();
+
+        System.out.println("sie haben "+ nameProdukt2 );
+
+
+
+
+
+
+      //  System.out.println(getPrice("Brot", produkts ));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//        double preisGesammt;
+//        preisGesammt = (preisgesammtWurst + preisgesammtKaese + preisgesammtBrot + preisgesammtDVD);
+//
+//
+//        if (preisGesammt > inhaltBrieftache) {
+//
+//            System.out.println("-----------------------------------------------------------");
+//            System.out.println(" ihnen fehlen " + (preisGesammt - inhaltBrieftache) + " EUR um diese Artikel zu Kaufen");
+//            System.out.println("-----------------------------------------------------------");
+//
+//        } else {
+//
+//            System.out.println("---------------------Beleg---------------------");
+//
+//            System.out.println("Wurst" + "   " + anzahlWurst + " x " + preisWurst + " EUR");
+//            System.out.println("                     " + preisgesammtWurst + " EUR");
+//
+//            System.out.println("Kaese" + "   " + anzahlKaese + " x " + preisKaese + " EUR");
+//            System.out.println("                     " + preisgesammtKaese + " EUR");
+//
+//            System.out.println("Brot " + "   " + anzahlBrot + " x " + preisBrot + " EUR");
+//            System.out.println("                     " + preisgesammtBrot + " EUR");
+//
+//            System.out.println("DVD  " + "   " + anzahlDVD + " x " + preisDVD + " EUR");
+//            System.out.println("                     " + preisgesammtDVD + " EUR");
+//
+//            System.out.println("----------------------------------------------");
+//
+//            System.out.println("Gesamt " + "              " + preisGesammt + " EUR");
+//            System.out.println("Gegeben" + "              " + inhaltBrieftache + " EUR");
+//            System.out.println(" ");
+//            System.out.println("Zurueck" + "              " + (inhaltBrieftache - preisGesammt) + " EUR");
+
+
     }
+
+    static Double getPrice(String name, List<Produkt> produkts){
+
+        for ( int i = 0 ; i < produkts.size() ; i ++ ) {
+
+            Produkt produkt = produkts.get(i);
+
+            if( produkt.getName().equals(name)){
+
+                return produkt.getPreis();
+            }
+        }
+        return null;
+    }
+
 }
 
 
---
+++ src/Produkt.java
@@ -0,0 +1,26 @@
+public class Produkt {
+
+    String name;
+    Double preis;
+
+    public Produkt(String name, Double preis) {
+        this.name = name;
+        this.preis = preis;
+    }
+
+    public Double getPreis() {
+        return preis;
+    }
+
+    public void setPreis(Double preis) {
+        this.preis = preis;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
