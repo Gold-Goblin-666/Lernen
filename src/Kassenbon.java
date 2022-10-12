f3bbdd1fb8e208cefd8ecf064f0fab95b9dff929
Laptop 2 <niki.wie.2000@gmail.com>
2022-10-12 15:09:19 +0200

    12.10.2022

--- src/Kassenbon.java
+++ src/Kassenbon.java
@@ -23,14 +23,12 @@ public class Kassenbon {
         String eur = " EUR";
 
 
-
-
         //##########################################################################################################################################
         // Aufgabe:
         //Wenn string zu lang für eine Spalte,
         //diesen auf mehrere Spalten aufteilen
 
-        // Scheuen wie viel plate in spalte, ist
+        // Scheuen wie viel Platz in spalte, ist
         // Zeichenanzahl im String durch Zeichen frei in Spalte => auf geraden teil aufrunden = Spalten die ich brauche
         // String mit Substring auf die Spalten aufzeieln
         // z.b. 0 bis 50; 51 bis 100; 101 bis rest ( 3 Zeielen mit z.B. gesammt 128 Zeichen
@@ -39,13 +37,13 @@ public class Kassenbon {
 
         System.out.println("Kassenbon" + '\n' + "kasse 1 " + '\n' + datumzeit + '\n' + "----------------------------------------------------------------");
 
+
         for (int i = 0; i < positionen.size(); i++) {
 
 
             System.out.println("################################################################");
 
             String produkthier = positionen.get(i).getProdukt().getName();
-
 
             if (produkthier.length() < 25) {
 
@@ -55,17 +53,25 @@ public class Kassenbon {
 
                 System.out.println(produkthier);
 
+                String zeichenkette = produkthier;
+                Boolean isFirstLine = true;
+                while (zeichenkette.length() > 0) {
+                    int split_index = getSplit(zeichenkette, 32);
+                    String print_line = zeichenkette.substring(0, split_index);
+                    zeichenkette = zeichenkette.substring(split_index);
+                    if (isFirstLine) {
+                        System.out.println("Anz * " + print_line + " 100.00 EUR");
+                        isFirstLine = false;
+                    } else {
+                        System.out.println(print_line);
+                    }
+                }
+
                 for (int n = 0; n < produkthier.length(); n++) {
                     if (Character.isSpaceChar(produkthier.charAt(n))) {
                         System.out.println(n);
                     }
                 }
-
-
-                System.out.println(produkthier.length());
-
-
-
 /*
                 int m;
 
@@ -92,46 +98,45 @@ public class Kassenbon {
             }
 
 
-                System.out.println("################################################################");
+            System.out.println("################################################################");
 
 
-                System.out.print(positionen.get(i) + " ");
+            System.out.print(positionen.get(i) + " ");
 
-                Rechnungsposition position = positionen.get(i);
+            Rechnungsposition position = positionen.get(i);
 
-                double preis = position.getPrice();
+            double preis = position.getPrice();
 
-                String rundenpreis = String.format("%.2f", preis);
+            String rundenpreis = String.format("%.2f", preis);
 
-                System.out.print(rundenpreis + eur);
-
-
-                double preisMehrere = preis * position.anzahl;
-
-                String rundenMehrere = String.format("%.2f", preisMehrere);
-
-                String preisLaenge = Double.toString(preis);
-
-                String positoinenString = positionen.get(i).toString();
-
-                int length = rundenMehrere.length();
-                int lenghtpreis = rundenpreis.length();
-                int lenghtPositionen = positoinenString.length();
-                int eurlenght = eur.length();
+            System.out.print(rundenpreis + eur);
 
 
-                int leerzeichenMitte = breitebon - 4 - 1 - lenghtPositionen - lenghtpreis - length - eurlenght;
+            double preisMehrere = preis * position.anzahl;
 
-                String z = "";
+            String rundenMehrere = String.format("%.2f", preisMehrere);
 
-                for (int b = 0; b < leerzeichenMitte; b++) {
-                    z = z + " ";
-                }
-                System.out.println(z + rundenMehrere + eur);
+            String preisLaenge = Double.toString(preis);
 
-                gesammt = gesammt + preisMehrere;
+            String positoinenString = positionen.get(i).toString();
 
+            int length = rundenMehrere.length();
+            int lenghtpreis = rundenpreis.length();
+            int lenghtPositionen = positoinenString.length();
+            int eurlenght = eur.length();
+
+
+            int leerzeichenMitte = breitebon - 4 - 1 - lenghtPositionen - lenghtpreis - length - eurlenght;
+
+            String z = "";
+
+            for (int b = 0; b < leerzeichenMitte; b++) {
+                z = z + " ";
             }
+            System.out.println(z + rundenMehrere + eur);
+
+            gesammt = gesammt + preisMehrere;
+
 
             System.out.println("----------------------------------------------------------------");
 
@@ -141,7 +146,7 @@ public class Kassenbon {
 
             String t = "";
 
-            for (int i = 0; i < leerzeichenEnde; i++) {
+            for (int k = 0; k < leerzeichenEnde; k++) {
                 t = t + " ";
             }
 
@@ -152,6 +157,32 @@ public class Kassenbon {
 
             return gesammt;
         }
+
+    }
+
+    private int getSplit(String zeichenkette, int i) {
+
+        String produkthier = positionen.get(i).getProdukt().getName();
+
+
+        for (int n = 0; n < produkthier.length(); n++) {
+            if (Character.isSpaceChar(produkthier.charAt(n))) {
+                System.out.println(n);
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+    }
 
 }
 //128: "--------------------------------------------------------------------------------------------------------------------------------"
