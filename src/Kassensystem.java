a72eb81a702dbac2637baec9b60da199c1d0ecc9
Laptop 2 <niki.wie.2000@gmail.com>
2022-10-19 15:38:58 +0200

    SQL datenbank

--- src/Kassensystem.java
+++ src/Kassensystem.java
@@ -1,5 +1,8 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -8,6 +11,18 @@ import java.util.Scanner;
 public class Kassensystem {
 
     public static void main(String[] args) {
+
+        String url = "jdbc:mysql://localhost:3306/test";
+        String username = "java";
+        String password = "123";
+
+        System.out.println("Connecting database...");
+
+        try (Connection connection = DriverManager.getConnection(url, username, password)) {
+            System.out.println("Database connected!");
+        } catch (SQLException e) {
+            throw new IllegalStateException("Cannot connect the database!", e);
+        }
 
         Scanner scanner = new Scanner(System.in);
 
